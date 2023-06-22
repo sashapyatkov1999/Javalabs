983881b9f222818b5ad202ad1b88d1b5f4de3492
skidrow <skidrow1999@bk.ru>
2023-06-22 22:18:07 +0300

    lab 1 and 2

---
+++ src/LAB2/task1/Calculator.java
@@ -0,0 +1,51 @@
+package LAB2.task1;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public long add(long a, long b) {
+        return a + b;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public long subtract(long a, long b) {
+        return a - b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public long multiply(long a, long b) {
+        return a * b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public long divide(long a, long b) {
+        return a / b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+}
