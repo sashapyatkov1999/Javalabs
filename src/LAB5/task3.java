05abedf96466568ab28b5707a2e43e3bd06aa9d2
skidrow <skidrow1999@bk.ru>
2023-07-10 18:40:36 +0300

    lab 6

--- src/LAB5/task3.java
+++ src/LAB5/task3.java
@@ -4,9 +4,14 @@ import java.util.Locale;
 
 public class task3 {
     public static void main(String[] args) {
-        System.out.println(censored("Бяка Евгеньевич"));
+        String original = "Бяка Евгеньевич";
+        String wordToReplace = "бяка";
+        String replacement = "«[вырезано цензурой]»";
+        String censoredText = censored(original, wordToReplace, replacement);
+        System.out.println(censoredText);
     }
-    public static String censored(String byaka){
-        return byaka.toLowerCase(Locale.ROOT).replace("бяка", "«[вырезано цензурой]»");
+
+    public static String censored(String original, String wordToReplace, String replacement) {
+        return original.toLowerCase(Locale.ROOT).replace(wordToReplace.toLowerCase(Locale.ROOT), replacement);
     }
 }
