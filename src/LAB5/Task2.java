b881fd657f515cdd59c9fddf9cf67b74a8bdddda
skidrow <skidrow1999@bk.ru>
2023-07-10 18:30:17 +0300

    lab 5,6

--- src/LAB5/Task2.java
+++ src/LAB5/Task2.java
@@ -1,14 +1,16 @@
 package LAB5;
 
+import java.util.Locale;
+
 public class Task2 {
     //2.Написать метод, который проверяет является ли слово палиндромом.
     public static void main(String[] args) {
-        System.out.println(iSPolindrom("радар"));
+        System.out.println(iSPolindrom("Топот"));
 
     }
     public static boolean iSPolindrom(String word){
-        String s = new StringBuilder(word).reverse().toString();
-        return s.equals(word);
+        String s = new StringBuilder(word).reverse().toString().toLowerCase(Locale.ROOT);
+        return s.equals(word.toLowerCase(Locale.ROOT));
     }
 
 }
